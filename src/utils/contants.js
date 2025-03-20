b281e4516dc4304e9f71c70910e07fa9a9215378
shimna-2 <shimnanarayanan2002@gmail.com>
2025-03-20 19:45:41 +0530

    created live chat

---
+++ src/utils/contants.js
@@ -0,0 +1,15 @@
+const GOOGLE_API_KEY = "AIzaSyDADHtcRWsMpkL73Ph3jCNZIW-OCxTJQDE";
+
+export const LIVE_CHAT_COUNT = 25;
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  GOOGLE_API_KEY;
+
+export const YOUTUBE_SEARCH_API = `https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
+
+//  const data= fetch("http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=", {
+//     mode: "no-cors",
+//   })
+
+// Live Chat >>>> Infinite Scroll >>>>>> Pagination
